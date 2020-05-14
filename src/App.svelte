e480ed51fe0800ccd68c87590bdb894d2ae47a20
John Tajima <manjiro@gmail.com>
2020-05-14 13:37:26 -0400

    update simple svelte-sandbox starter. Remove bootstrap as a requirement, just link stylesheet

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from "./Button.svelte";
+  import interact from 'interactjs';
 </script>
 
 <style>
@@ -9,8 +10,14 @@
   }
 </style>
 
-<main>
+<main class='container'>
 	<h1>Hello CodeSandbox</h1>
 	<h2>Start editing to see some magic happen!</h2>
+
+  <h2>Hell world</h2>
+  <div class='alert alert-success'>
+    <h4>Yes, it worked!</h4>
+  </div>
+
 	<Button />
 </main>
